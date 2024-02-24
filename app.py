e3469e9241db01f49d7a372202fc7c82d2771e3c
root <root@pps.myguest.virtualbox.org>
2024-02-24 16:33:00 +0100

    Ejercicio 7 resuelto, todo funciona correctamente.

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
+from flask import Flask, jsonify
 from bayeta import frotar
 
-if __name__ == "__main__":
-    frases_aleatorias = frotar(n_frases=3)
-    for frase in frases_aleatorias:
-        print(frase)
+app = Flask(__name__)
 
+@app.route('/frotar/<int:n_frases>')
+def obtener_frases(n_frases):
+	frases_aleatorias = frotar(n_frases)
+	print(frases_aleatorias)
+	return jsonify({'Frases auspiciosas:': frases_aleatorias})
+
+
+@app.route('/')
+def hello_world():
+    return '¡prueba!'
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
+
+if __name__ == '__main__':
+	main()
--- bayeta.py
+++ bayeta.py
@@ -1,14 +1,17 @@
 import random
+    # Tu código para generar frases aleatorias...
+def frotar (n_frases: int = 1) -> list():
+#	frases_aleatorias = frotar(n_frases)
+	res=[]
+	for frase in range(n_frases):
+		file=open("frases.txt","r")
+		num=random.randint(1,5)
+		for i in range(num):
+			linea=file.readline()
+		res.append(linea)
+		file.close()
+	return res
 
-def frotar(n_frases: int = 1) -> list:
-    #frases
-    lista_de_frases = [
-        "Diría que tienes buena suerte, pero no me caes muy bien, usuario anónimo.",
-        "Mala suerte para ti, por listo.",
-        "Tu suerte es nula.",
-    ]
+    # Resto de tu código...
 
-    frases_elegidas = random.sample(lista_de_frases, n_frases)
-
-    return frases_elegidas
-
+#if __name__ == "__main__":
